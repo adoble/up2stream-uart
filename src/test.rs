ab2498e243d7156de3959c826ba58f84459c3aa7
Andrew Doble <andrew.l.doble@gmail.com>
2023-07-09 15:41:57 +0200

    Working test send_command

--- src/test.rs
+++ src/test.rs
@@ -1,7 +1,24 @@
-#[allow(warnings)]
+//#[allow(warnings)]
+//use embedded_hal::blocking::serial::Write;
+use embedded_hal_mock::serial::{Mock as SerialMock, Transaction as SerialTransaction};
+//use embedded_hal_mock::serial::{Read, Write};
+//use embedded_hal_mock::MockError;
+
 use super::*;
 
 #[test]
-fn dummy_test() {
-    assert!(true);
+fn send_command() {
+    let msg = "CMD:on;".as_bytes();
+    let expectations = [
+        SerialTransaction::write_many(msg),
+        SerialTransaction::flush(),
+    ];
+
+    let mut serial = SerialMock::new(&expectations);
+
+    let mut up2stream_device = Up2Stream::new(&mut serial);
+
+    let _result = up2stream_device.send_command("CMD", "on").unwrap();
+
+    serial.done();
 }
