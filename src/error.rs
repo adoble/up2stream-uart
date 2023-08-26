04e8dfd614e559aa7a4c7dbbdeabbfd61d2e74f6
Andrew Doble <andrew.l.doble@gmail.com>
2023-08-26 15:52:53 +0200

    Removed conditional compilation

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,16 @@
 #![allow(unused_imports)]
 
+// TODO reinstate the conditional compilation, but this time
+// use falgs to th compiler. Just handling the condition
+// not(test) does not include conditions such as doctest.
+// Need to provide docu on this in the readme.
+
 // Do not include this when testing in std environment
-#[cfg(not(test))]
-use defmt::Format;
+// #[cfg(not(test))]
+// use defmt::Format;
 
 #[derive(Debug)]
-#[cfg_attr(not(test), derive(defmt::Format))] // Only used when running on target hardware
+//#[cfg_attr(not(test), derive(defmt::Format))] // Only used when running on target hardware
 pub enum Error {
     NotSupportedForDeviceSource,
     ReadingQueryResponse,
